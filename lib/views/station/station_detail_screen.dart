955a0e0df8aac398ff1d7022915515177f70d5e1
Tejas Galande <tejas5galande@gmail.com>
2025-10-10 20:57:12 +0530

    add station detail screen and provider

--- lib/views/station/station_detail_screen.dart
+++ lib/views/station/station_detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ev_point/controllers/station/station_detail_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StationDetailScreen extends StatefulWidget {
   const StationDetailScreen({super.key});
@@ -10,8 +12,29 @@ class StationDetailScreen extends StatefulWidget {
 class _StationDetailScreenState extends State<StationDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Station Detail"),),
+    return ChangeNotifierProvider(
+      create: (context) => StationDetailProvider(),
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text("Expandable App Bar"),
+                background: Image.network(
+                  "https://via.placeholder.com/300",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            
+          ],
+        ),
+      ),
     );
   }
 }
